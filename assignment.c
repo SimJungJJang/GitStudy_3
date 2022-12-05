3a2e002996b1b20befcbee74c4cb1dcdfdb0da49
Wontiger <624tiger@naver.com>
2022-12-05 19:04:57 +0900

    pull&request by wonho_kim

---
+++ assignment.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double Max(double *, int);
+
+int main(void)
+{
+	double ary1[5] = { 5.3, 1.4, 7.9, 0.4, -4.5 };
+	double ary2[3] = { -4.3, -1.5, -10.7 };
+
+	printf("ary1의 최댓값 : %f \n", Max(ary1, 5));
+	printf("ary2의 최댓값 : %f \n", Max(ary2, 3));
+	return 0;
+}
+
+double Max(double *a, int b)
+{
+	double max = a[0];
+
+	for (int i = 0; i < b; i++)
+	{
+		if (max < a[i])
+		{
+			max = a[i];
+		}
+	}
+	return max;
+}
